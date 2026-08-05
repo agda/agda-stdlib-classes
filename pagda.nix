256704b3a727ab76393ad4b26589d17c9e98bb4f
whatisRT <andre.knispel@gmx.de>
2026-08-05 14:27:49 +0200

    Add `pagda`

---
+++ pagda.nix
@@ -0,0 +1,7 @@
+{ pkgs, pagda }:
+{
+  meta = {
+    description = "Extension of Agda's standard library to accommodate a more typeclass-friendly interface.";
+    homepage = "https://github.com/agda/agda-stdlib-classes";
+  };
+}
